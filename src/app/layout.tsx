f5f782f107c1612fcbdfd7903bea116fa68798c7
diodzi <me@diodzi.dev>
2025-08-23 20:24:06 -0400

    removed font, changed favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import ObserverProvider from '@/components/intersect-observer-provider'
 import { Toaster } from '@/components/ui/sonner'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
   title: 'Dio Dziban',
@@ -27,6 +16,10 @@ export default function RootLayout({
   return (
     <html lang='en' suppressHydrationWarning>
       <head>
+        <link
+          rel='icon'
+          href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌃</text></svg>'
+        />
         <link rel='stylesheet' href='https://use.typekit.net/pgi1ucm.css' />
       </head>
       <ObserverProvider>
